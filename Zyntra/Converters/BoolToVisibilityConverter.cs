f9e22e5568a8b5b6d2218b008e7666f3d9ca26bb
coder-isxt <robloxsaksuproporo@gmail.com>
2026-04-12 13:33:52 +0300

    Release v26.4.13

--- Zyntra/Converters/BoolToVisibilityConverter.cs
+++ Zyntra/Converters/BoolToVisibilityConverter.cs
@@ -68,3 +68,16 @@ public class NonZeroToVisibilityConverter : IValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
+
+public class FirstCharConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string s && s.Length > 0)
+            return s[0].ToString();
+        return "?";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
